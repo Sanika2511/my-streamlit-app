6cc65cacb67deb027dda6e7a2c83ace4d09bf9bc
Sanika Sharma <129964625+Sanika2511@users.noreply.github.com>
2025-05-29 19:40:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import streamlit as st
 import pandas as pd
 import pickle
+import os
+import requests
+
+# Google Drive direct download links with file IDs extracted from your URLs
+MOVIES_URL = "https://drive.google.com/uc?export=download&id=1NdM9oOzUxihNngjGaQsHT_42wUasqBAB"
+SIMILARITY_URL = "https://drive.google.com/uc?export=download&id=1AeWcfVrj8f9EcK1Xt79pq_mZLILh_kzZ"
+
+# Filenames to save locally
+MOVIES_FILE = "movies.pkl"
+SIMILARITY_FILE = "similarity.pkl"
+
+def download_file(url, filename):
+    if not os.path.exists(filename):
+        with st.spinner(f"Downloading {filename}..."):
+            response = requests.get(url)
+            if response.status_code == 200:
+                with open(filename, "wb") as f:
+                    f.write(response.content)
+            else:
+                st.error(f"Failed to download {filename}!")
+                st.stop()
+
+# Download files if not present
+download_file(MOVIES_URL, MOVIES_FILE)
+download_file(SIMILARITY_URL, SIMILARITY_FILE)
 
 # Load saved pickle files
-movies = pickle.load(open('movies.pkl', 'rb'))
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+movies = pickle.load(open(MOVIES_FILE, 'rb'))
+similarity = pickle.load(open(SIMILARITY_FILE, 'rb'))
 
 # Set page config
 st.set_page_config(page_title="Movie Recommender", layout="centered")
@@ -37,4 +56,3 @@ if st.button("Recommend"):
         st.success("Here are 5 movies you might enjoy:")
         for i, title in enumerate(recommendations, start=1):
             st.write(f"{i}. {title}")
-
